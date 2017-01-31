14f0f240cbb91f3a54bc1cc77aea517b4b69c563
joshkirsh720 <joshua.kirshenbaum@gmail.com>
2017-01-31 14:22:24 -0500

    Setup
    Initial Setup

---
+++ Store Project/src/Item.java
@@ -0,0 +1,13 @@
+
+public class Item {
+	String name, description;
+	double price;
+	int quantity;
+	
+	public Item(String n, String d, double p, int q) {
+		name = n;
+		description = d;
+		price = p;
+		quantity = q;
+	}
+}
